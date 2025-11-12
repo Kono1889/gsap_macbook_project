21908b7d9357902e0b0d77dacedaa2d8a0165b3f
Kono1889 <agyemangkono1889@gmail.com>
2025-11-12 11:09:07 +0000

    implemented productview page and included a 3d model

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import ProductViewer from "./components/ProductViewer";
 
 const App = () => {
   return (
-    <main><Navbar/>
-    <Hero />
+    <main>
+      <Navbar />
+      <Hero />
+      <ProductViewer />
     </main>
   );
 };

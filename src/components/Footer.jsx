f29bf83cf9d7112590fcc080aee05f4f92eef2a8
Kono1889 <agyemangkono1889@gmail.com>
2025-11-14 16:25:47 +0000

    footer implemented

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import { footerLinks } from "../constants";
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <footer>
+      <div className="info">
+        <p>
+          More ways to shop: Find Apple Store or other retailer near you or call
+          +233 20 3695211.
+        </p>
+        <img src="/logo.svg" alt="Apple logo" />
+
+        <hr />
+
+        <div className="links">
+          <p>Copyright 2025 Apple Inc. All rights reserved.</p>
+
+          <ul>
+            {footerLinks.map(({label, link}) => (
+              <li key={label}>
+                <a href={link}>{label}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </footer>
   );
-}
+};
 
 export default Footer;
